8fb98a1fb34509fc7418bb656122f189f7d78b4d
Samuel Ayanleke <dayoayanleke96@gmail.com>
2026-09-07 18:03:57 +0100

    I'm done with exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,42 @@
-using System;
-
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
+        Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter;
+        if (grade >= 90)
+        
+{
+        letter = "A";
+}
+else if (grade >= 80)
+{
+    letter = "B";
+}
+else if (grade >= 70)
+{
+    letter = "C";
+}
+else if (grade >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
+
+Console.WriteLine($"Your grade is: {letter}");
+
+if (grade >= 70)
+{
+    Console.WriteLine("Congratulations! You passed the course.");
+}
+else
+{
+    Console.WriteLine("Keep working hard and try again next time.");
+}
+}
 }
